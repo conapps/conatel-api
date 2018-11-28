d3d078764ead38859917fd2c3e458f5aaf22e45d
agusnavce <agus-navce52@hotmail.com>
2018-11-28 13:59:31 -0300

    Added new login

--- src/auth/controller.py
+++ src/auth/controller.py
@@ -2,6 +2,7 @@ import os
 import requests
 import jwt
 from flask import Blueprint, redirect, request, jsonify
+from oauth2client.client import OAuth2Credentials
 from src import oidc
 from src.utils import error
 from config import APP_DIR
@@ -13,8 +14,23 @@ pem_path = os.path.join(APP_DIR, 'key.pem')
 
 
 @auth.route('/login')
-@oidc.require_login
 def login():
+    return oidc.redirect_to_auth_server(None, request.values)
+
+
+@auth.route('/callback')
+@oidc.custom_callback
+def callback(data):
+    info = oidc.user_getinfo(['sub'])
+    user_id = info.get('sub')
+    if user_id in oidc.credentials_store:
+        try:
+            access_token = OAuth2Credentials.from_json(
+                oidc.credentials_store[user_id]).access_token
+            print ('access_token=<%s>' % access_token)
+            headers = {'Authorization': 'Bearer %s' % (access_token)}
+        except:
+            a = 2
     return redirect('http://google.com', code=302)
 
 
